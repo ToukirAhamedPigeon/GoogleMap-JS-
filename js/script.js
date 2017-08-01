6cf7b2bc76190e4109269e007fc49066902a50c5
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@hotmail.com>
2017-08-02 01:23:43 +0600

    2nd commit
    Show map

---
+++ js/script.js
@@ -0,0 +1,7 @@
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 23.777176, lng: 90.399452},
+          zoom: 12
+        });
+      }
