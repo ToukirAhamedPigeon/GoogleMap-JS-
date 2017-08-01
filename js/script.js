e892c92e520f661de58360cddd61981f112d3c79
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@hotmail.com>
2017-08-02 02:02:23 +0600

    2nd commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 23.777176, lng: 90.399452},
-          zoom: 12
+          zoom: 12,
+		  //disableDefaultUI:true,
+		  //scrollwheel:false,
+		  //mapTypeId:google.maps.MapTypeId.ROADMAP
+		  mapTypeId:google.maps.MapTypeId.HYBRID
+		  //mapTypeId:google.maps.MapTypeId.SATELLITE
         });
       }
